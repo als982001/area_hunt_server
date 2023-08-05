4f55379514663ac7ce9f5929c3fab7566372da5c
주재민 <als982001@naver.com>
2023-08-05 22:07:18 +0900

    Feat: corsOptions의 주소 중 슬래시 제거

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(cookieParser());
 const corsOptions = {
   origin: [
     "http://localhost:3000",
-    "http://areahunt.s3-website.ap-northeast-2.amazonaws.com/",
+    "http://areahunt.s3-website.ap-northeast-2.amazonaws.com",
   ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   credentials: true,
