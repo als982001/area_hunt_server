8e864139d5f205a2430e8608fb0582f0a404a845
주재민 <als982001@naver.com>
2023-08-10 20:03:22 +0900

    Test: 테스트 용으로 domain 추가03

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -17,7 +17,7 @@ const codes = {
 };
 
 const cookieOption = {
-  domain: "http://localhost:3000",
+  domain: "http://localhost",
   path: "/",
   httpOnly: true,
   sameSite: "none",
@@ -25,7 +25,7 @@ const cookieOption = {
 };
 
 const refreshCookieOption = {
-  domain: "http://localhost:3000",
+  domain: "http://localhost",
   path: "/",
   httpOnly: true,
   sameSite: "none",
